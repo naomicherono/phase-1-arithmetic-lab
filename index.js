d95de64dcfa0e550b7b04e1c7ff1f5ff9642ff4c
naomicherono <nonniecheronoh12@gmail.com>
2023-06-14 15:11:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const random = Math.floor(Math.random() * 10) + 1;
+const max = Math.max(5, 10, 15, 20);
+
+console.log(multiply); 
+console.log(random); 
+console.log(mod); 
+console.log(max);
